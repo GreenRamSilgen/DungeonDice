4a753f69a42277a93e11afab380ca53539857ebb
GreenRamSilgen <kiranshrs@yahoo.com>
2026-01-17 13:35:39 -0800

    Initial Commit

---
+++ DungeonDice/DungeonDiceApp.swift
@@ -0,0 +1,17 @@
+//
+//  DungeonDiceApp.swift
+//  DungeonDice
+//
+//  Created by Kiran Shrestha on 1/17/26.
+//
+
+import SwiftUI
+
+@main
+struct DungeonDiceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
