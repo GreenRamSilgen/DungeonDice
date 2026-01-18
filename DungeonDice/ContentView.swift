71c8a5ba02928b2ecb19b0f026b46f060d2153e6
GreenRamSilgen <kiranshrs@yahoo.com>
2026-01-18 09:37:12 -0800

    Ch 4.4 LazyGrids, Ch 4.5 Identifiable Protocol

--- DungeonDice/ContentView.swift
+++ DungeonDice/ContentView.swift
@@ -8,7 +8,7 @@
 import SwiftUI
 
 struct ContentView: View {
-    enum Dice : Int, CaseIterable {
+    enum Dice : Int, CaseIterable, Identifiable {
         case four = 4
         case six = 6
         case eight = 8
@@ -16,6 +16,10 @@ struct ContentView: View {
         case twelve = 12
         case twenty = 20
         case hundred = 100
+        
+        var id: Int {
+            rawValue
+        }
         
         func roll() -> Int {
             Int.random(in: 1...self.rawValue)
@@ -41,8 +45,8 @@ struct ContentView: View {
                 .frame(height: 150)
             
             Spacer()
-            Group {
-                ForEach(Dice.allCases, id: \.self) { dice in
+            LazyVGrid(columns: [GridItem(.adaptive(minimum: 102))]) {
+                ForEach(Dice.allCases) { dice in
                     Button("\(dice.rawValue)-sided") {
                         resultMessage = "You rolled a \(dice.roll()) on a \(dice.rawValue)-sided dice"
                     }
