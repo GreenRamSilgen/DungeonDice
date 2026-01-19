bce6180862bb91e4aa82ffa1c11d74a9ada8336f
GreenRamSilgen <kiranshrs@yahoo.com>
2026-01-19 07:30:33 -0800

    Ch 4.6 adding animation to rotate view

--- DungeonDice/ContentView.swift
+++ DungeonDice/ContentView.swift
@@ -17,9 +17,8 @@ struct ContentView: View {
         case twenty = 20
         case hundred = 100
         
-        var id: Int {
-            rawValue
-        }
+        var id: Int { rawValue }
+        var description : String { "\(rawValue)-sided" }
         
         func roll() -> Int {
             Int.random(in: 1...self.rawValue)
@@ -27,7 +26,8 @@ struct ContentView: View {
     }
     
     @State private var resultMessage = ""
-    @State private var players = ["Elle", "Mike", "Will", "Lucas", "Sam", "Dustin" ]
+    @State private var animationTrigger = false
+    @State private var isDoneAnimating = true
     
     var body: some View {
         VStack {
@@ -42,13 +42,24 @@ struct ContentView: View {
                 .font(.largeTitle)
                 .fontWeight(.medium)
                 .multilineTextAlignment(.center)
+//                .scaleEffect(isDoneAnimating ? 1.0 : 0.6)
+//                .opacity(isDoneAnimating ? 1.0 : 0.2)
                 .frame(height: 150)
+                .rotation3DEffect(isDoneAnimating ? .degrees(720) : .degrees(0), axis: (x: 0, y: 1, z: 1))
+                .onChange(of: animationTrigger) {
+                    isDoneAnimating = false
+                    
+                    withAnimation(.interpolatingSpring(duration: 0.6, bounce: 0.4)) {
+                        isDoneAnimating = true
+                    }
+                }
             
             Spacer()
             LazyVGrid(columns: [GridItem(.adaptive(minimum: 102))]) {
                 ForEach(Dice.allCases) { dice in
-                    Button("\(dice.rawValue)-sided") {
+                    Button(dice.description) {
                         resultMessage = "You rolled a \(dice.roll()) on a \(dice.rawValue)-sided dice"
+                        animationTrigger.toggle()
                     }
                 }
             }
